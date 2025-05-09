40b791d3f87f5bd1dded0abbd397105cc4b80823
haitd5 <haitran1606@gmail.com>
2025-05-09 14:18:48 +0700

    done

---
+++ 8 - AJAX/ajax.js
@@ -0,0 +1,3 @@
+// UI = User Interface                          => user tuong tac voi system thong qua giao dien
+// CLI = CommandLine Interface                  => user tuong tac voi system thong qua dong lenh
+// API = Application Programming Interface      => system tuong tac voi system
