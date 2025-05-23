cec0da0b403600be53845482c346f2a186ef94be
haitd5 <haitran1606@gmail.com>
2025-05-23 11:33:31 +0700

    done

---
+++ 9 - react-webpack/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function App() {
+    return (
+        <div>
+            <h1 className="heading_1">React Webpack</h1>
+            <a href="#">Home Page</a>
+            <h3>React Js</h3>
+        </div>
+    );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
