abab08acbd1ce41d2ae336b8c01d1a0960f88da3
haitd5 <haitran1606@gmail.com>
2025-05-26 13:36:32 +0700

    done

--- 9 - react-webpack/src/index.js
+++ 9 - react-webpack/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import flower from "../src/assets/flower.jpeg";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <h1 className="heading_1">React Webpack</h1>
             <a href="#">Home Page</a>
             <h3>React Js</h3>
+            <img src={flower} alt="" className="img-md" />
         </div>
     );
 }
